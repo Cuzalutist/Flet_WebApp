44c6e8062b047d8cc0da862861490edba94b906c
Kushal <kusalku@gmail.com>
2023-06-23 22:00:44 +0545

    Some additional changes.

--- testREST.py
+++ testREST.py
@@ -3,7 +3,7 @@ import json
 
 # Base URL and resource path
 base_url = "http://192.168.1.78:8980/REST_EMMService/rest/REST_EMMService"
-resource_path = "/Coils"
+resource_path = "/UsersMenuList"
 
 # Request headers
 headers = {
@@ -18,8 +18,9 @@ if response.status_code == 200:
     # try:
         # Extract the output parameter from the response
     response_json = response.json()
-    vOut = response_json['response']['ttAllCoils']['ttAllCoils'][0]
-    print("Coil name:", vOut['coilName'])
+    vOut = response_json['response']['userMenuList']['userMenuList'][1]
+    print("User Code:", vOut['ttUserCode'])
+    print("User Program:", vOut['ttUserName'])
     # except KeyError as e:
     #     print("KeyError:", str(e))
     #     print("Response JSON:", response_json)
